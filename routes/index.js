8eb9beb30cc568f80dc244b3b2eed685388a53b3
Yejin15 <dpwls273@naver.com>
2018-02-10 23:09:18 +0900

    /talk 추가

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,32 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/talk', function(req, res, next) {
+
+    const messages = [
+        {
+            name: 'Bot name',
+            text: 'Hello!   Thanks for connecting with us over Messenger.',
+            type : 'receive'
+        },
+        {
+            name: 'Bot name',
+            text: 'Are you a bot or real person?   ' +
+            'Let me guess :)',
+            type : 'send'
+        },
+        {
+            name: 'Bot name',
+            text: 'The Messenger Platform provides you with tools to ' +
+            'create a bot to engage in conversation with people.',
+            type : 'receive'
+        }
+    ];
+
+
+    res.render('messenger', {messages: messages});
+
+
+});
+
 module.exports = router;
