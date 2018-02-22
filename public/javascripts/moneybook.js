e36a2f0aacef229bf55e7fa4ec959097e3580644
h.hyunwoo@gmail.com <Angles12>
2018-02-22 20:08:39 +0900

    account book logic 수정

--- public/javascripts/moneybook.js
+++ public/javascripts/moneybook.js
@@ -1,62 +1,63 @@
-
 var $body = $('.body');
-var $data = $body.find('.data');
-var $row = $data.find('.row');
+var $data = $body.find('#data');
+var datas = [];
+var $date = $('#date');
+var $income = $('#income');
+var $expense = $('#expense');
 
-var totalincome = 0;
-var totalexpense = 0;
-var totalmoney = 0;
-
-var incomeData = [];
-var expenseData = [];
-var rowdata = [];
+// 버튼이 눌리면, 가계부에 입력한다
 
 $('.inputbar').find('.button').on('click', function () {
 
+  var date = $date.val();
+  var income = $income.val();
+  var expense = $expense.val();
 
-     var date = $('#date').val();
-     var income = $('#income').val();
-     incomeData.push(income);
-     var expense = $('#expense').val();
-     expenseData.push(expense);
+  var currentData = {
+    date: date,
+    income: income,
+    expense: expense,
+  };
+  datas.push(currentData);
 
-     totalincome = totalincome*1 + income*1;
-     totalexpense = totalexpense*1 + expense*1;
-     totalmoney = totalincome*1 - totalexpense*1;
+  var $item = $("<div class='row'>" +
+    "<div class='cells'> " + currentData.date + " </div>" +
+    "<div class='cells income'> " + currentData.income + " 원</div>" +
+    "<div class='cells expense'> " + currentData.expense + " 원</div>" +
+    "<div class='cells'> " + (currentData.income - currentData.expense) + " 원</div>" +
+    "<div class='close'> " + "<div class='X'>  X  <div>" +
+    " </div>"
+    + " </div>");
+  $item.appendTo($data);
 
+  calcSums();
 
-
-    $("<div class='row'>" +
-        "<div class='cells1'> " +  date  +
-        " </div>" +
-        "<div class='cells2'> " +  income  +
-        " 원</div>" +
-        "<div class='cells3'> " +  expense +
-        " 원</div>" +
-        "<div class='cells4'> " +  (income-expense) +
-        " 원</div>"
-        +
-        "<div class='close'> " +  "<div class='X'>  X  <div>" +
-        " </div>"
-         +" </div>").appendTo($data);
-
-    $("#inTotal").html(totalincome + "원");
-    $("#exTotal").html(totalexpense + "원");
-    $("#Total").html(totalmoney + "원");
-
-
-
+  // x를 누르면 가계부에서 삭제한다.
+  $item.find('.close').on('click', function () {
+    // datas 에서 currentData 제거해야한다.
+    var currentIndex = datas.indexOf(currentData);
+    datas.splice(currentIndex, 1);
+    $item.remove();
+    calcSums();
+  });
 });
 
-var incomeX = 0;
 
-$data.on("click", '.row',function() {
+function calcSums() {
+  var totalIncome = _.sumBy(datas, function (d) {
+    return d.income * 1;
+  });
+  var totalExpense = _.sumBy(data, function (d) {
+    return d.expense * 1;
+  });
+
+  $('#inTotal').text(totalIncome + '원');
+  $('#exTotal').text(totalExpense + '원');
+  $('#Total').text(totalIncome - totalExpense + '원');
+};
 
 
-    this.remove();
-    $("#inTotal").html(totalincome + "원");
-    $("#exTotal").html(totalexpense + "원");
-    $("#Total").html(totalmoney + "원");
+// 가격을 갱신하는 기능
 
 
-});
+
